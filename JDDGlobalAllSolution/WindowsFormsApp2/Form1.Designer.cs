ea9ba67dc6cd0640208632dc05296c6c1999b26e
jdalmar <42837970+jdalmar@users.noreply.github.com>
2018-08-31 12:55:03 +0100

    Update Form1.Designer.cs

--- JDDGlobalAllSolution/WindowsFormsApp2/Form1.Designer.cs
+++ JDDGlobalAllSolution/WindowsFormsApp2/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(621, 356);
-            this.button1.Name = "button1";
+            this.button1.Name = "buttonGIT";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
